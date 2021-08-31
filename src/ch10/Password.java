0d9ef3c7fc7e208da443ca4554800c11df92a2bf
qhdltmwhs <qhdltmwhs@gmail.com>
2021-08-31 14:28:47 +0900

    .

---
+++ src/ch10/Password.java
@@ -0,0 +1,28 @@
+package ch10;
+
+public class Password {
+
+	private String password;
+
+	public Password() {
+
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) throws PasswordException {
+
+		if (password == null) {
+			throw new PasswordException("비밀번호는 null 일 수 없습니다.");
+		} else if (password.length() < 5) {
+			throw new PasswordException("비밀번호는 5자 이상이어야 합니다.");
+		} else if (password.matches("[a-zA-Z]+")) {
+			throw new PasswordException("비밀번호는 숫자나 특수문자를 포함해야 합니다.");
+		}
+		this.password = password;
+
+	}
+
+}
---
+++ src/ch10/PasswordTest.java
@@ -0,0 +1,43 @@
+package ch10;
+
+public class PasswordTest {
+
+	public static void main(String[] args) {
+
+		Password test = new Password();
+
+		String password = null;
+		try {
+			test.setPassword(password);
+			System.out.println("오류 없음1");
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+
+		password = "abcd";
+		try {
+			test.setPassword(password);
+			System.out.println("오류 없음2");
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+
+		password = "abcde";
+		try {
+			test.setPassword(password);
+			System.out.println("오류 없음3");
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+
+		password = "abcde#1";
+		try {
+			test.setPassword(password);
+			System.out.println("오류 없음4");
+		} catch (PasswordException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
