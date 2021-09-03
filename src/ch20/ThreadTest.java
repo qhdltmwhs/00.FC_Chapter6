6e0a0752b0b3c601cd10bbc36cd123a066479638
qhdltmwhs <qhdltmwhs@gmail.com>
2021-09-03 18:47:50 +0900

    .

--- src/ch20/ThreadTest.java
+++ src/ch20/ThreadTest.java
@@ -1,7 +1,7 @@
 package ch20;
 
 class MyThread extends Thread {
-	
+
 	@Override
 	public void run() {
 		for (int i = 0; i < 200; i++) {
@@ -14,17 +14,27 @@ class MyThread extends Thread {
 public class ThreadTest {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(Thread.currentThread() + " start");
 		System.out.println("extends Thread");
-		
+
 		MyThread th1 = new MyThread();
 		th1.start();
 
 		MyThread th2 = new MyThread();
 		th2.start();
-
+		
 		System.out.println(Thread.currentThread() + " end");
+		
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.print("[[anonymous]]\t");
+			}
+
+		};
+		runnable.run();
 
 	}
 
